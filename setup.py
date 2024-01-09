bb4915f77a18eb40bcfa74788a07f021f9d546ff
lolesia <alesyavaskovith@gmail.com>
2024-01-09 14:02:33 +0200

    rebild structure

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='nasa_open_api',
     version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(include=['core', 'nasa_api']),
     description='Receive and manage data from two endpoints Nasa Open APi',
     install_requires=open('requirements.txt').read().strip().split('\n'),
 )
